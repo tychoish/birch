c99c1050a00ee13ca3f9f6e25272f14778864b79
tycho garen <garen@tychoish.com>
2023-01-29 13:26:50 -0500

    benchmark cases

--- x/bench/bson_test.go
+++ x/bench/bson_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"testing"
 
+	ebirch "github.com/evergreen-ci/birch"
 	lbson "github.com/globalsign/mgo/bson"
 	"github.com/tychoish/birch"
 	"github.com/tychoish/fun"
@@ -45,134 +46,213 @@ func BenchmarkBSON(b *testing.B) {
 	output := fun.Must(bson.Marshal(input))
 	jsonOut := fun.Must(json.Marshal(input))
 	var err error
-	b.Run("Marshaling", func(b *testing.B) {
-		b.Run("Reference", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				output, err = bson.Marshal(input)
-				if err != nil || len(output) == 0 {
-					b.Fatal()
+	b.Run("Marshal", func(b *testing.B) {
+		b.Run("Baseline", func(b *testing.B) {
+			b.Run("BSON", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = bson.Marshal(input)
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
 				}
-			}
-		})
-		b.Run("MGO", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				output, err = lbson.Marshal(input)
-				if err != nil || len(output) == 0 {
-					b.Fatal()
+			})
+			b.Run("MGO", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = lbson.Marshal(input)
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
 				}
-			}
-		})
-		b.Run("JSON", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				output, err = json.Marshal(input)
-				if err != nil || len(output) == 0 {
-					b.Fatal()
+			})
+			b.Run("JSON", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = json.Marshal(input)
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
 				}
-			}
+			})
 		})
-		b.Run("BirchConstructor", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
+		b.Run("Birch", func(b *testing.B) {
+			b.Run("Constructor", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					doc := birch.DC.MapString(input)
+					if doc.Len() != 100 {
+						b.Fatal()
+					}
+				}
+			})
+			b.Run("Marshaler", func(b *testing.B) {
 				doc := birch.DC.MapString(input)
-				if doc.Len() != 100 {
-					b.Fatal()
+				b.ResetTimer()
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = doc.MarshalBSON()
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
 				}
-			}
+			})
+			b.Run("Combined", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = birch.DC.MapString(input).MarshalBSON()
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
+				}
+			})
 		})
-		b.Run("BirchMarshaler", func(b *testing.B) {
-			doc := birch.DC.MapString(input)
-			b.ResetTimer()
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				output, err = doc.MarshalBSON()
-				if err != nil || len(output) == 0 {
-					b.Fatal()
+		b.Run("EVG", func(b *testing.B) {
+			b.Run("Constructor", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					doc := ebirch.DC.MapString(input)
+					if doc.Len() != 100 {
+						b.Fatal()
+					}
 				}
-			}
-		})
-		b.Run("BirchCombined", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				output, err = birch.DC.MapString(input).MarshalBSON()
-				if err != nil || len(output) == 0 {
-					b.Fatal()
+			})
+			b.Run("Marshaler", func(b *testing.B) {
+				doc := ebirch.DC.MapString(input)
+				b.ResetTimer()
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = doc.MarshalBSON()
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
 				}
-			}
+			})
+			b.Run("Combined", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					output, err = ebirch.DC.MapString(input).MarshalBSON()
+					if err != nil || len(output) == 0 {
+						b.Fatal()
+					}
+				}
+			})
 		})
 	})
-	b.Run("Unmarshaling", func(b *testing.B) {
-		b.Run("JSON", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := map[string]string{}
-				err = json.Unmarshal(jsonOut, &resolved)
-				if err != nil || len(resolved) != 100 {
-					b.Fatal(err, len(resolved))
-				}
-			}
-		})
-		b.Run("Reference", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := map[string]string{}
-				err = bson.Unmarshal(output, &resolved)
-				if err != nil || len(resolved) != 100 {
-					b.Fatal(err, len(resolved))
-				}
-			}
-		})
-		b.Run("MGO", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := map[string]string{}
-				err = bson.Unmarshal(output, &resolved)
-				if err != nil || len(resolved) != 100 {
-					b.Fatal(err, len(resolved))
-				}
-			}
-		})
-		b.Run("BirchExport", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := birch.DC.Reader(output).ExportMap()
-				if err != nil || len(resolved) != 100 {
-					b.Fatal(err, len(resolved))
-				}
-			}
-		})
-		b.Run("BirchNoExport", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := birch.DC.ReadFrom(bytes.NewBuffer(output))
-				if err != nil || resolved.Len() != 100 {
-					b.Fatal(err, resolved.Len())
-				}
-			}
-		})
-		b.Run("BirchPrealloc", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := birch.DC.Make(100)
-				err := resolved.UnmarshalBSON(output)
-				if err != nil || resolved.Len() != 100 {
-					b.Fatal(err, resolved.Len())
-				}
-			}
-		})
-		b.Run("BirchDirect", func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				resolved := birch.DC.New()
-				err := resolved.UnmarshalBSON(output)
-				if err != nil || resolved.Len() != 100 {
-					b.Fatal(err, resolved.Len())
-				}
-			}
-		})
+	b.Run("Unmarshal", func(b *testing.B) {
 
+		b.Run("Baseline", func(b *testing.B) {
+			b.Run("BSON", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := map[string]string{}
+					err = bson.Unmarshal(output, &resolved)
+					if err != nil || len(resolved) != 100 {
+						b.Fatal(err, len(resolved))
+					}
+				}
+			})
+
+			b.Run("JSON", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := map[string]string{}
+					err = json.Unmarshal(jsonOut, &resolved)
+					if err != nil || len(resolved) != 100 {
+						b.Fatal(err, len(resolved))
+					}
+				}
+			})
+			b.Run("MGO", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := map[string]string{}
+					err = bson.Unmarshal(output, &resolved)
+					if err != nil || len(resolved) != 100 {
+						b.Fatal(err, len(resolved))
+					}
+				}
+			})
+		})
+		b.Run("Birch", func(b *testing.B) {
+			b.Run("Export", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := birch.DC.Reader(output).ExportMap()
+					if err != nil || len(resolved) != 100 {
+						b.Fatal(err, len(resolved))
+					}
+				}
+			})
+			b.Run("NoExport", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := birch.DC.ReadFrom(bytes.NewBuffer(output))
+					if err != nil || resolved.Len() != 100 {
+						b.Fatal(err, resolved.Len())
+					}
+				}
+			})
+			b.Run("Prealloc", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := birch.DC.Make(100)
+					err := resolved.UnmarshalBSON(output)
+					if err != nil || resolved.Len() != 100 {
+						b.Fatal(err, resolved.Len())
+					}
+				}
+			})
+			b.Run("Direct", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := birch.DC.New()
+					err := resolved.UnmarshalBSON(output)
+					if err != nil || resolved.Len() != 100 {
+						b.Fatal(err, resolved.Len())
+					}
+				}
+			})
+		})
+		b.Run("EVG", func(b *testing.B) {
+			b.Run("Export", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := ebirch.DC.Reader(output).ExportMap()
+					if err != nil || len(resolved) != 100 {
+						b.Fatal(err, len(resolved))
+					}
+				}
+			})
+			b.Run("NoExport", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := ebirch.DC.ReadFrom(bytes.NewBuffer(output))
+					if err != nil || resolved.Len() != 100 {
+						b.Fatal(err, resolved.Len())
+					}
+				}
+			})
+			b.Run("Prealloc", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := ebirch.DC.Make(100)
+					err := resolved.UnmarshalBSON(output)
+					if err != nil || resolved.Len() != 100 {
+						b.Fatal(err, resolved.Len())
+					}
+				}
+			})
+			b.Run("Direct", func(b *testing.B) {
+				b.ReportAllocs()
+				for i := 0; i < b.N; i++ {
+					resolved := ebirch.DC.New()
+					err := resolved.UnmarshalBSON(output)
+					if err != nil || resolved.Len() != 100 {
+						b.Fatal(err, resolved.Len())
+					}
+				}
+			})
+		})
 	})
-
 }
--- x/bench/go.mod
+++ x/bench/go.mod
@@ -9,6 +9,8 @@ require (
 )
 
 require (
+	github.com/evergreen-ci/birch v0.0.0-20230104155808-09f0a18c134f // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tychoish/fun v0.3.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
